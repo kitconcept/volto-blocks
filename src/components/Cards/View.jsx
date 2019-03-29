58675e9a70cad732143c347ea42079ac6ba326c1
Victor Fernandez de Alba <sneridagh@gmail.com>
2019-03-29 16:44:28 +0100

    Improve cards by using the Edit Tile in detached mode.

---
+++ src/components/Cards/View.jsx
@@ -0,0 +1,68 @@
+/**
+ * View image tile.
+ * @module components/manage/Tiles/Slider/View
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Card, Image } from 'semantic-ui-react';
+import redraft from 'redraft';
+import cx from 'classnames';
+import { settings } from '~/config';
+import { flattenToAppURL } from '@plone/volto/helpers';
+
+/**
+ * View image tile class.
+ * @class View
+ * @extends Component
+ */
+const View = ({ data }) => (
+  <div
+    className={cx('tile cards', {
+      centered: data.align === 'center' || data.align === undefined,
+      'space-between': data.align === 'space-between',
+      'centered-text': data.centeredText,
+    })}
+  >
+    <Card.Group
+      className={cx({
+        centered: data.align === 'center' || data.align === undefined,
+        'space-between': data.align === 'space-between',
+      })}
+      itemsPerRow={data.expandCards ? data.cards.length : 4}
+    >
+      {data.cards.map(card => (
+        <Card key={card.id} className={cx({ 'no-borders': data.noBorders })}>
+          <Image
+            src={
+              card.url.startsWith(settings.apiPath)
+                ? `${flattenToAppURL(card.url)}/@@images/image`
+                : card.url
+            }
+          />
+          {!data.hideText && (
+            <Card.Content>
+              {card.text &&
+                redraft(
+                  card.text,
+                  settings.ToHTMLRenderers,
+                  settings.ToHTMLOptions,
+                )}
+            </Card.Content>
+          )}
+        </Card>
+      ))}
+    </Card.Group>
+  </div>
+);
+
+/**
+ * Property types.
+ * @property {Object} propTypes Property types.
+ * @static
+ */
+View.propTypes = {
+  data: PropTypes.objectOf(PropTypes.any).isRequired,
+};
+
+export default View;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import CardsEditTile from './Cards/Edit';
+import CardsViewTile from './Cards/View';
+import SliderEditTile from './Slider/Edit';
+import SliderViewTile from './Slider/View';
+
+export { CardsEditTile, CardsViewTile, SliderEditTile, SliderViewTile };
