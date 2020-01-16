e1676a36222a66337c1ef43726b155df903e8c4d
Victor Fernandez de Alba <sneridagh@gmail.com>
2020-01-16 19:24:36 +0100

    backport querysearch fixes for relative path criterion from Volto

--- src/actions/querystringsearch/querystringsearch.js
+++ src/actions/querystringsearch/querystringsearch.js
@@ -1,19 +1,33 @@
 const GET_QUERYSTRING_RESULTS = 'GET_QUERYSTRING_RESULTS';
 
 /**
- * Get breadcrumbs.
- * @function getBreadcrumbs
- * @param {string} query Query.
- * @returns {Object} Get querystring results action.
+ * Get querystring results.
+ * @function getQueryStringResults
+ * @param {Object} data Data.
+ * @returns {Object} Get querystringsearch results action.
  */
-export function getQueryStringResults(data, subrequest) {
+export function getQueryStringResults(path, data, subrequest) {
+  // fixes https://github.com/plone/volto/issues/1059
+  const fixedQuery = data?.query?.map(queryElem => {
+    if (queryElem.o === 'plone.app.querystring.operation.string.relativePath') {
+      return {
+        ...queryElem,
+        v: `${path}/${queryElem.v}`,
+      };
+    }
+    return queryElem;
+  });
+
   return {
     type: GET_QUERYSTRING_RESULTS,
     subrequest,
     request: {
       op: 'post',
       path: '/@querystring-search',
-      data,
+      data: {
+        ...data,
+        query: fixedQuery,
+      },
     },
   };
 }
