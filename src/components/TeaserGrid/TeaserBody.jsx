b14cf1a6e0f1d8a9064afa6d477617f4fbdd1322
Victor Fernandez de Alba <sneridagh@gmail.com>
2020-11-13 17:54:52 +0100

    Impove the getImage URL func

--- src/components/TeaserGrid/TeaserBody.jsx
+++ src/components/TeaserGrid/TeaserBody.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Message } from 'semantic-ui-react';
 import { defineMessages, useIntl } from 'react-intl';
 import imageBlockSVG from '@plone/volto/components/manage/Blocks/Image/block-image.svg';
-import { flattenToAppURL } from '@plone/volto/helpers';
+import { flattenToAppURL, isInternalURL } from '@plone/volto/helpers';
 
 const messages = defineMessages({
   PleaseChooseContent: {
@@ -16,6 +16,12 @@ const messages = defineMessages({
 
 function getImageURL(data) {
   if (
+    typeof data.preview_image === 'object' &&
+    (data.preview_image['content-type'] === 'image/gif' ||
+      data.preview_image['content-type'] === 'image/svg+xml')
+  ) {
+    return flattenToAppURL(data.preview_image.download);
+  } else if (
     data.preview_image.scales?.teaser.download &&
     typeof data.preview_image === 'object'
   ) {
@@ -26,7 +32,11 @@ function getImageURL(data) {
     // We've manually overriden the image pointing to an image content type,
     // then we have a string, we get it via URL shorthand
     // TODO: get the actual image scale UUID for better caching
-    return flattenToAppURL(`${data.preview_image}/@@images/image/teaser`);
+    if (isInternalURL(data.preview_image)) {
+      return flattenToAppURL(`${data.preview_image}/@@images/image/teaser`);
+    } else {
+      return data.preview_image;
+    }
   } else {
     // Guard for edge cases
     return flattenToAppURL(`${data.href}/@@images/preview_image/teaser`);
