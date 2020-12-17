69db0d081988a282548e55a8c4ba3909036144f5
Victor Fernandez de Alba <sneridagh@gmail.com>
2020-12-17 14:18:56 +0100

    Create content is not used anywhere

--- src/components/Grid/Edit.jsx
+++ src/components/Grid/Edit.jsx
@@ -52,7 +52,6 @@ class Edit extends Component {
     onFocusPreviousBlock: PropTypes.func.isRequired,
     onFocusNextBlock: PropTypes.func.isRequired,
     handleKeyDown: PropTypes.func.isRequired,
-    createContent: PropTypes.func.isRequired,
     gridType: PropTypes.string,
     templates: PropTypes.func.isRequired,
     sidebarData: PropTypes.func.isRequired,
