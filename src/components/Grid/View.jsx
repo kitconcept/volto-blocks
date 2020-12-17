01da2c2eb4bce3ce90477864870fe9d6fb57009c
iFlameing <ialokkumarsingh0@gmail.com>
2020-12-17 16:56:10 +0530

    Adding view for variation

--- src/components/Grid/View.jsx
+++ src/components/Grid/View.jsx
@@ -2,29 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid } from 'semantic-ui-react';
 import cx from 'classnames';
+import { BlockWrapperEnhancer } from '@kitconcept/volto-blocks/components';
 
 const View = ({ data, render, path }) => {
   return (
-    <div
-      className={cx('block __grid', {
-        [data['@type']]: true,
-        centered: data.align === 'center' || data.align === undefined,
-        'space-between': data.align === 'space-between',
-        'centered-text': data.centeredText,
-        one: data.columns.length === 1,
-        two: data.columns.length === 2,
-        three: data.columns.length === 3,
-        four: data.columns.length === 4,
-      })}
-    >
-      <Grid stackable columns={data.columns.length}>
-        {data.columns.map((column) => (
-          <Grid.Column key={column.id}>
-            {render({ ...column, block: column.id }, path, data.columns)}
-          </Grid.Column>
-        ))}
-      </Grid>
-    </div>
+    <BlockWrapperEnhancer data={data}>
+      <div
+        className={cx('block __grid', {
+          [data['@type']]: true,
+          centered: data.align === 'center' || data.align === undefined,
+          'space-between': data.align === 'space-between',
+          'centered-text': data.centeredText,
+          one: data.columns.length === 1,
+          two: data.columns.length === 2,
+          three: data.columns.length === 3,
+          four: data.columns.length === 4,
+        })}
+      >
+        <Grid stackable columns={data.columns.length}>
+          {data.columns.map((column) => (
+            <Grid.Column key={column.id}>
+              {render({ ...column, block: column.id }, path, data.columns)}
+            </Grid.Column>
+          ))}
+        </Grid>
+      </div>
+    </BlockWrapperEnhancer>
   );
 };
 
--- src/components/SchemaRenderer/SchemaRenderer.jsx
+++ src/components/SchemaRenderer/SchemaRenderer.jsx
@@ -87,24 +87,27 @@ const InlineForm = ({
       <div id={`blockform-fieldset-${defaultFieldset.id}`}>
         <Segment className="form attached">
           {map(defaultFieldset.fields, (field, index) => (
-            <Field
-              {...schema.properties[field]}
-              id={fieldIndex !== undefined ? `${field}-${fieldIndex}` : field}
-              fieldSet={defaultFieldset.title.toLowerCase()}
-              focus={index === 0}
-              value={schema.properties[field].value || formData[field]}
-              required={schema.required.indexOf(field) !== -1}
-              onChange={(id, value) => {
-                const name =
-                  fieldIndex !== undefined
-                    ? id.replace(`-${fieldIndex}`, '')
-                    : id;
-                onChangeField(name, value);
-              }}
-              key={field}
-              error={errors[field]}
-              block={block}
-            />
+            <>
+              {console.log(schema.properties[field])}
+              <Field
+                {...schema.properties[field]}
+                id={fieldIndex !== undefined ? `${field}-${fieldIndex}` : field}
+                fieldSet={defaultFieldset.title.toLowerCase()}
+                focus={index === 0}
+                value={schema.properties[field].value || formData[field]}
+                required={schema.required.indexOf(field) !== -1}
+                onChange={(id, value) => {
+                  const name =
+                    fieldIndex !== undefined
+                      ? id.replace(`-${fieldIndex}`, '')
+                      : id;
+                  onChangeField(name, value);
+                }}
+                key={field}
+                error={errors[field]}
+                block={block}
+              />
+            </>
           ))}
         </Segment>
       </div>
