f916f0e6872398c73890f4a8373f2f6f38759a06
Victor Fernandez de Alba <sneridagh@gmail.com>
2020-12-21 18:21:06 +0100

    Fix tests

--- jest-addon.config.js
+++ jest-addon.config.js
@@ -15,4 +15,13 @@ module.exports = {
     'load-volto-addons':
       '<rootDir>/node_modules/@plone/volto/jest-addons-loader.js',
   },
+  transform: {
+    '^.+\\.js(x)?$': 'babel-jest',
+    '^.+\\.css$': 'jest-css-modules',
+    '^.+\\.less$': 'jest-css-modules',
+    '^.+\\.scss$': 'jest-css-modules',
+    '^.+\\.(png)$': 'jest-file',
+    '^.+\\.(jpg)$': 'jest-file',
+    '^.+\\.(svg)$': './jest-svgsystem-transform.js',
+  },
 };
